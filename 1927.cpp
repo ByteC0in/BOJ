e18d667f54c1f909e17041482080b698d79825a6
ByteC0in <107418351+ByteC0in@users.noreply.github.com>
2022-06-14 00:09:10 +0900

    Create 1927.cpp

---
+++ 1927.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <functional>
+#include <algorithm>
+#include <ctime>
+#include <stack>
+#include<queue>
+using namespace std;
+
+int main()
+{
+    vector<int> ans;
+    int n;
+    priority_queue<int, vector<int>, greater<int>> pq;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int s;
+        cin >> s;
+        if (s != 0)
+        {
+            pq.push(s);
+        }
+        else
+        {
+            if (pq.empty())ans.push_back(0);
+            else
+            {
+                ans.push_back(pq.top());
+                pq.pop();
+            }
+        }
+    }
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << "\n";
+    }
+     return 0;
+}
