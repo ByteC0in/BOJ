3a5353bbe4a3e95d81e7192ffa34d8f4ce63155c
ByteC0in <107418351+ByteC0in@users.noreply.github.com>
2022-06-13 23:59:24 +0900

    Create 1001.cpp

---
+++ 1001.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    float a;
+    float b;
+    std::cin >> a;
+    std::cin >> b;
+    float answer = a - b;
+    std::cout << answer;
+    return 0;
+}
