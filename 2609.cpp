49216580d5b433ded0834e928de56b5952202609
ByteC0in <107418351+ByteC0in@users.noreply.github.com>
2022-06-14 23:38:34 +0900

    Create 2609.cpp

---
+++ 2609.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <set>
+ 
+ using namespace std;
+
+ 
+ int main()
+ {
+     long long A;
+     std::cin >> A;
+     long long B;
+     std::cin >> B;
+     long long r;
+     long long a = A;
+     long long b = B;
+     while (b != 0)
+     {
+
+         r = a % b;
+         a = b;
+         b = r;
+     }
+     long long answer = (A / a)*(B / a)*a;
+     std::cout << a<<"\n";
+     std::cout << answer;
+     return 0;
+}  
