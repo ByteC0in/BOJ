8ed368071acee70b5e132350b8dab61b36f9dfc5
ByteC0in <107418351+ByteC0in@users.noreply.github.com>
2022-06-14 23:54:32 +0900

    Create 9316.cpp

---
+++ 9316.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+#include <math.h>
+#include <vector>
+#include <string>
+#include <functional>
+#include <algorithm>
+#include <ctime>
+#include <stack>
+#include<queue>
+#include <cstdio>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        cout << "Hello World, Judge " << i << "!\n";
+    }
+    return 0;
+}
