1b13b76d98ec257b7dcf6f23d9fa159624b5d7fd
ByteC0in <107418351+ByteC0in@users.noreply.github.com>
2022-06-14 23:35:27 +0900

    Create 2558.cpp

---
+++ 2558.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <functional>
+#include <algorithm>
+#include <ctime>
+#include <stack>
+#include<queue>
+#include <cstdio>
+using namespace std;
+int s = 0;
+int main()
+{
+    int a,b;
+    cin >> a>>b;
+    cout << a + b;
+    return 0;
+}
