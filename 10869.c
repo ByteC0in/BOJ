49d0a4e3ea015e24c842b8644cfe336f572abe57
ByteC0in <107418351+ByteC0in@users.noreply.github.com>
2022-06-15 14:08:25 +0900

    Create 10869.c

---
+++ 10869.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int s;
+    int n;
+    scanf("%d%d",&s,&n);
+    printf("%d\n",s+n);
+    printf("%d\n",s-n);
+    printf("%d\n",s*n);
+    printf("%d\n",s/n);
+    printf("%d\n",s%n);
+    return 0;
+}
