2f41e2f0adabfc9f9d4b06a472cc05930afcef79
ByteC0in <107418351+ByteC0in@users.noreply.github.com>
2022-06-15 14:16:41 +0900

    Create 11654.cpp

---
+++ 11654.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+int main()
+{
+    string s;
+    std::cin >> s;
+    int a = s.at(0);
+    std::cout << a;
+    std::cin >> s;
+    return 0;
+}
