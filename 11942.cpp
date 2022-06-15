039b627c611ee9b48226033e29f82ef8bf20b51c
ByteC0in <107418351+ByteC0in@users.noreply.github.com>
2022-06-15 14:20:16 +0900

    Create 11942.cpp

---
+++ 11942.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+#include <math.h>
+#include <vector>
+#include <string>
+#include <functional>
+#include <algorithm>
+#include <ctime>
+#include <stack>
+#include<queue>
+#include <cstdio>
+using namespace std;
+int main()
+{
+    cout << "고려대학교";
+
+    return 0;
+}
