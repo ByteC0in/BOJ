6e62d218ddc2f17bb101c64009836072239217f6
ByteC0in <107418351+ByteC0in@users.noreply.github.com>
2022-06-15 14:12:33 +0900

    Create 10998.cpp

---
+++ 10998.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <functional>
+#include <algorithm>
+#include <ctime>
+#include <stack>
+#include<queue>
+#include <cstdio>
+using namespace std;
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << a * b;
+     return 0;
+}
