a1de76294623995ff71afe642f5c4c6da150eb3b
ByteC0in <107418351+ByteC0in@users.noreply.github.com>
2022-06-15 14:10:45 +0900

    Create 10951.cpp

---
+++ 10951.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <functional>
+#include <algorithm>
+#include <ctime>
+#include <stack>
+#include<queue>
+#include <cstdio>
+using namespace std;
+
+
+int main()
+{
+    int A, B; 
+    while (cin >> A >> B) 
+    {
+        cout << A + B << '\n'; 
+    }
+    return 0;
+}
